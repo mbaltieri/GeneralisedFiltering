59035713dacc84bc338e363420768f574a992b80
Manuel Baltieri <manuel.baltieri@gmail.com>
2020-05-09 01:41:29 +0900

    Test going smoothly so far, now in need of a way to map forward/backward passes to forward mode/inversion of a generative model

--- TestModelClass.py
+++ TestModelClass.py
@@ -1,5 +1,9 @@
 from Model import *
 
+dt = 1
+T = 1
+iterations = int(T/dt)
+
 l = 3                               # number of layers
 
 m = 2                               # observations dimension
@@ -8,10 +12,10 @@ r = 2                               # inputs dimension
 p = 6                               # parameters dimension
 h = 3                               # hyperparameters dimension
 
-e_n = 4                             # embedding dimension hidden states
-e_r = 4                             # embedding dimension inputs
-e_p = 1                             # embedding dimension parameters
-e_h = 1                             # embedding dimension hyperparameters
+e_n = 3                             # embedding dimension hidden states
+e_r = 0                             # embedding dimension inputs
+e_p = 0                             # embedding dimension parameters
+e_h = 0                             # embedding dimension hyperparameters
 
 delta = 1.50                                                # parameters simulating a simple mass-spring system as the environment
 epsilon = 15.0
@@ -19,37 +23,18 @@ epsilon = 15.0
 A = torch.tensor([[0, 1], [delta, epsilon]])
 F = torch.tensor([[1, 0], [1, 0], [1, 0], [1, 0]])
 
-C = torch.tensor([[3., 0.], [0., 1.]])
-H = torch.tensor([[2., 0.], [0., 1.], [.5, 0.], [.25, 1.]])                       # TODO: tensor type deault is 'int64' but need float for pseudo-inverse
+Sigma_w = torch.tensor([[3., 0.], [0., 1.]])
+Sigma_z = torch.tensor([[2., 0., 0., 0.], [0., 1., 0., 0.], [0., 0, .5, 0.], [0., 0., 0., .25]])                       # TODO: tensor type deault is 'int64' but need float for pseudo-inverse
 
-model = layer(A=A, F=F, C=C, H=H, e_n=e_n)
+GP = layer(A=A+10, F=F, Sigma_w=Sigma_w, Sigma_z=Sigma_z, e_n=e_n)
+GM = layer(A=A, F=F, Sigma_w=Sigma_w, Sigma_z=Sigma_z, e_n=e_n)
 
-b = model.A @ model.x
-# print(b)
-# print(Diff(b.double(), 2, e_n+1))
+for i in range(iterations):
+    GP.forward()
+    GM.updateObservations(GP.y)
 
-model.prediction_errors()
+    GM.backward()
 
-free = model.free_energy()
+    GM.forward(flag = 0)
 
-# print(model.F.size())
-# print(model.x.size())
-
-# print(model.G.size())
-# print(model.v.size())
-
-# print(model.H.size())
-# print(model.z.size())
-
-# print(model.C.size())
-# print(model.w.size())
-
-
-# print(model.n)
-# print(model.w)
-
-a = model.f()
-c = model.g()
-
-# print(model.H)
-# print(model.C)
+    
