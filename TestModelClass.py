0b871df89aec39171d8e02e5bb69800719b94668
mbaltieri <manuel.baltieri@gmail.com>
2020-12-30 20:13:21 +0900

    Automatic differentiation is working. Filtering is working. Generalised coordinates are working.
    The code is increadibly slow for long sequences (something with generalised coordinates of the noise is badly implemented, i.e., hugely unoptimised).
    
    The process is heavily dependent on learning rates, need to get rid of this dependence. Ideally using this local linearisation method that Friston uses.

--- TestModelClass.py
+++ TestModelClass.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 
 # torch.autograd.set_detect_anomaly(True)
 
-dt = .5
-T = 3
+dt = .01
+T = 30
 iterations = int(T/dt)
 
 l = 3                               # number of layers
@@ -16,7 +16,7 @@ r = 2                               # inputs dimension
 p = 6                               # parameters dimension
 h = 3                               # hyperparameters dimension
 
-e_n = 1                             # embedding dimension hidden states
+e_n = 2                             # embedding dimension hidden states
 e_r = 0                             # embedding dimension inputs
 e_p = 0                             # embedding dimension parameters
 e_h = 0                             # embedding dimension hyperparameters
@@ -26,17 +26,22 @@ epsilon = -10.0
 
 A = torch.tensor([[0, 1], [delta, epsilon]], device=DEVICE)
 F = torch.tensor([[1, 0], [1, 0], [1, 0], [1, 0]], device=DEVICE)
+F = torch.tensor([[1, 0], [0, 1]], device=DEVICE)
 
-Sigma_w = torch.tensor([[0., 0.], [0., .1]], device=DEVICE)
+Sigma_w = torch.tensor([[0., 0.], [0., .0001]], device=DEVICE)
 Sigma_z = torch.tensor([[2., 0., 0., 0.], [0., 1., 0., 0.], [0., 0, .5, 0.], [0., 0., 0., .25]], device=DEVICE)                       # TODO: tensor type deault is 'int64' but need float for pseudo-inverse
+Sigma_z = torch.tensor([[.1, 0], [0, .1]], device=DEVICE)
 
 Sigma_w_GM = torch.tensor([[.1, 0.], [0., .1]], device=DEVICE)
 
-GP = layer(T, dt, A=A, F=F, Sigma_w=Sigma_w, Sigma_z=Sigma_z, e_n=e_n, history=iterations)            # TODO: At the moment simulating only sequencies with Brownian motion, 
+GP = layer(T, dt, A=A, F=F, Sigma_w=Sigma_w_GM, Sigma_z=Sigma_z, e_n=e_n, history=iterations)            # TODO: At the moment simulating only sequencies with Brownian motion, 
                                                                             # since Gaussian autocorrelations introduce negative covariances that 
                                                                             # can't truly be simulated (as far as I know), Karl has a way to build 
                                                                             # generalised sequencies out of this so check spm
 GM = layer(T, dt, A=A, F=F, Sigma_w=Sigma_w_GM, Sigma_z=Sigma_z, e_n=e_n, history=iterations)
+
+learning_rate = 5e-3
+learning_rate = 0.03
 
 for i in range(iterations-1):
     print(i)
@@ -45,53 +50,78 @@ for i in range(iterations-1):
     # GM.setObservations(GP.y.detach())
     GM.setObservations(GP.y)
 
-    # GM.generalisedCoordinates()
     F = GM.free_energy(i)
-    
-    # retain gradients for intermediate variables
-    # TODO: find a better way to deal with this
-    # GM.x.retain_grad()
-    # GM.v.retain_grad()
 
-
-    # F.backward(retain_graph=True)                             # TODO: is retain_graph=True necessary? Or even correct?
+    # Use autograd to compute the backward pass.
     F.backward()
 
-    # dFdx = GM.x.grad
-    # dFdv = GM.v.grad
-
-    # # update variables
-    # GM.x += dt * dFdx
-    # GM.v += dt * dFdv
+    # Update weights using gradient descent
+    # print(GM.y)
+    # print(GM.x)
     
-    # GM.save_history(i)
-    # GP.save_history(i)
+    dFdx = GM.x.grad
+    # print(GM.y)
+    # print(GM.x)
+    # print(dFdx)
+    # dFdv = GM.v.grad
+    # print(GM.Sigma_w)
+    # print(GM.Pi_w)
+    with torch.no_grad():
+        GM.x -= learning_rate * dt * dFdx
+        # GM.v -= learning_rate * dt * dFdv
 
+        # Manually zero the gradients after updating weights
+        GM.x.grad = None
+        # GM.v.grad = None
+    
+    GM.save_history(i)
+
+plt.figure()
+plt.plot(GM.F_history[:-1].detach())
 
 fig = plt.figure()
-ax1 = fig.add_subplot(151)
-ax2 = fig.add_subplot(152)
-ax3 = fig.add_subplot(153)
-ax4 = fig.add_subplot(154)
-ax5 = fig.add_subplot(155)
+ax1 = fig.add_subplot(121)
+ax2 = fig.add_subplot(122)
 
-ax1.plot(range(iterations-1), GP.y_history[:-1,0,0].detach(), 'b')
-ax1.plot(range(iterations-1), GM.x_history[:-1,0,0].detach(), 'r')
+# ax1 = fig.add_subplot(251)
+# ax2 = fig.add_subplot(252)
+# ax3 = fig.add_subplot(253)
+# ax4 = fig.add_subplot(254)
+# ax5 = fig.add_subplot(255)
 
-ax2.plot(range(iterations-1), GP.w_history[:-1,0,0].detach(), 'b')
-ax2.plot(range(iterations-1), GP.w_history[:-1,1,0].detach(), 'r')
+# ax7 = fig.add_subplot(257)
+# ax9 = fig.add_subplot(259)
 
-ax3.plot(range(iterations-1), GP.wSmoothened[0,:-1].detach(), 'b')
-ax3.plot(range(iterations-1), GP.wSmoothened[1,:-1].detach(), 'r')
+ax1.plot(range(iterations-1), GM.y_history[:-1,0].detach(), 'b')
+ax1.plot(range(iterations-1), GM.x_history[:-1,0].detach(), 'r')
+ax1.plot(range(iterations-1), GP.x_history[:-1,0].detach(), 'k')
 
-ax4.plot(range(iterations-1), GP.z_history[:-1,0,0].detach(), 'b')
-ax4.plot(range(iterations-1), GP.z_history[:-1,1,0].detach(), 'r')
-ax4.plot(range(iterations-1), GP.z_history[:-1,2,0].detach(), 'g')
-ax4.plot(range(iterations-1), GP.z_history[:-1,3,0].detach(), 'k')
+ax2.plot(range(iterations-1), GM.y_history[:-1,1].detach(), 'b')
+ax2.plot(range(iterations-1), GM.x_history[:-1,1].detach(), 'r')
+ax2.plot(range(iterations-1), GP.x_history[:-1,1].detach(), 'k')
 
-ax5.plot(range(iterations-1), GP.zSmoothened[0,:-1].detach(), 'b')
-ax5.plot(range(iterations-1), GP.zSmoothened[1,:-1].detach(), 'r')
-ax5.plot(range(iterations-1), GP.zSmoothened[2,:-1].detach(), 'g')
-ax5.plot(range(iterations-1), GP.zSmoothened[3,:-1].detach(), 'k')
+# ax2.plot(range(iterations-1), GP.w_history[:-1,0,0].detach(), 'b')
+# ax2.plot(range(iterations-1), GP.w_history[:-1,1,0].detach(), 'r')
+
+# ax7.plot(range(iterations-1), GP.w_history[:-1,2,0].detach(), 'b')
+# ax7.plot(range(iterations-1), GP.w_history[:-1,3,0].detach(), 'r')
+
+# ax3.plot(range(iterations-1), GP.wSmoothened[0,:-1].detach(), 'b')
+# ax3.plot(range(iterations-1), GP.wSmoothened[1,:-1].detach(), 'r')
+
+# ax4.plot(range(iterations-1), GP.z_history[:-1,0,0].detach(), 'b')
+# ax4.plot(range(iterations-1), GP.z_history[:-1,1,0].detach(), 'r')
+# ax4.plot(range(iterations-1), GP.z_history[:-1,2,0].detach(), 'g')
+# ax4.plot(range(iterations-1), GP.z_history[:-1,3,0].detach(), 'k')
+
+# ax9.plot(range(iterations-1), GP.z_history[:-1,4,0].detach(), 'b')
+# ax9.plot(range(iterations-1), GP.z_history[:-1,5,0].detach(), 'r')
+# ax9.plot(range(iterations-1), GP.z_history[:-1,6,0].detach(), 'g')
+# ax9.plot(range(iterations-1), GP.z_history[:-1,7,0].detach(), 'k')
+
+# ax5.plot(range(iterations-1), GP.zSmoothened[0,:-1].detach(), 'b')
+# ax5.plot(range(iterations-1), GP.zSmoothened[1,:-1].detach(), 'r')
+# ax5.plot(range(iterations-1), GP.zSmoothened[2,:-1].detach(), 'g')
+# ax5.plot(range(iterations-1), GP.zSmoothened[3,:-1].detach(), 'k')
 
 plt.show()
