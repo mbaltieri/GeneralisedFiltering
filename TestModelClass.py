Local linearisation implemented for the generative process and corrected a bug in the generative model inversion. The bug was due to an incorrect treatment of the Jacobian in the LL scheme derived from the differences of the current implementation (minimisation of F) as opposed to what is found in SPM (maximisation of -F).
Now we need to find the equation capturing updates for action.

(This integration scheme is really robust.)

TestModelClass.py
@@ -15,8 +15,8 @@ if torch.cuda.is_available():
 else:
     torch.set_default_tensor_type(torch.DoubleTensor)
 
-dt = .02
-T = 20
+dt = 1
+T = 100
 iterations = int(T/dt)
 
 l = 3                               # number of layers
@@ -39,6 +39,7 @@ e_h = 0                             # embedding dimension hyperparameters
 ## generative process
 
 A = torch.tensor([[0., 1., 0], [0., 0., 0.], [0., 0., 0.]], device=DEVICE)                 # state transition matrix
+# A = torch.tensor([[0., 1., 0.], [-2, -2, 0.], [0., 0., 0.]], device=DEVICE)
 B_a = torch.tensor([[0., 0., 0.], [0., 1., 0.], [0., 0., 0.]], device=DEVICE)               # input matrix (dynamics)
 F = torch.tensor([[1., 0., 0.], [0., 1., 0.], [0., 0., 1.]], device=DEVICE)               # observation matrix
 
@@ -85,9 +86,6 @@ GP = layer('GP', T, dt, A=A, F=F, Sigma_w=Sigma_w, Sigma_z=Sigma_z, e_n=e_n, B_a
 GM = layer('GM', T, dt, A=A_gm, F=F_gm, Sigma_w=Sigma_w_GM, Sigma_z=Sigma_z_GM, Sigma_v=Sigma_v_GM, e_n=e_n, dyda=dyda, B_u=B_u_gm, eta_u=eta_u)
 
 for i in range(1,iterations-1):
-    GP.saveHistoryVariables(i)
-    GM.saveHistoryVariables(i)
-
     print(i)
 
     GP.step(i)
@@ -107,20 +105,29 @@ for i in range(1,iterations-1):
 
     inputs = (GM.x, GM.u)
     
-    H = torch.autograd.functional.hessian(lambda x, v: GM.free_energy(x, v, i=i), inputs)
-    # print(H[0][0].shape)
-    H_x = H[0][0].squeeze(1).squeeze(2)
-    H_u = H[1][1].squeeze(1).squeeze(2)
+    J = torch.autograd.functional.hessian(lambda x, v: GM.free_energy(x, v, i=i), inputs)                         # TODO: wait for a decent implementation of 'hessian' and 'jacobian' on all inputs similar to backward
+    J_x = J[0][0].squeeze()                                                                                       # (at the moment both functions rely on grad, which requires specifying inputs). If not, to save some 
+    J_u = J[1][1].squeeze()                                                                                       # time, might want to switch backward --> grad and than take jacobian of grad
+
+    # print('J_x: ', J_x)
+    # print('J_u: ', J_u)
+    # print('expm(J_x): ', torch.matrix_exp(dt * -J_x))
 
     with torch.no_grad():
         # integrate using Euler-Maruyama
         # GM.x = GM.x + dt * (Diff(GM.x, GM.sim, GM.e_sim+1) - dFdx)
         # GM.u = GM.u + dt * (Diff(GM.u, GM.sim, GM.e_sim+1) - dFdu)
-        GP.a = GP.a + dt * (- dFda)
+        # GP.a = GP.a + dt * (- dFda)
+
+        # print(dFdx)
+        # print(dFdu)
 
         # integrate using Local-linearisation
-        dx = (torch.matrix_exp(dt * H_x) - torch.eye(GM.sim)) @ H_x.pinverse() @ (Diff(GM.x, GM.sim, GM.e_sim+1) - dFdx)
-        du = (torch.matrix_exp(dt * H_u) - torch.eye(GM.sim)) @ H_u.pinverse() @ (Diff(GM.u, GM.sim, GM.e_sim+1) - dFdu)
+        dx = (torch.matrix_exp(dt * -J_x) - torch.eye(GM.sim)) @ - J_x.pinverse() @ (Diff(GM.x, GM.sim, GM.e_sim+1) - dFdx)                 # NB: J --> - J since this is a minimisation of F, unlike the maximisation of -F in DEM and HMB
+        du = (torch.matrix_exp(dt * -J_u) - torch.eye(GM.sim)) @ - J_u.pinverse() @ (Diff(GM.u, GM.sim, GM.e_sim+1) - dFdu)
+
+        # print(dx)
+        # print(du)
 
         GM.x = GM.x + dt * dx
         GM.u = GM.u + dt * du
@@ -132,6 +139,8 @@ for i in range(1,iterations-1):
 
         GM.x.requires_grad = True                                               # FIXME: Definitely ugly, find a better way to deal with this (earlier, we had updates of the form GM.x[i+1] = GM.x[i] + ... 
         GM.u.requires_grad = True                                               # but this requires "i" as a parameter of the loss funciton, and "torch.autograd.functional.hessian" accepts only tensors as inputs)
+    GP.saveHistoryVariables(i)
+    GM.saveHistoryVariables(i)
 
 plt.figure()
 plt.plot(GM.y_history[:-1,0].detach(), GM.y_history[:-1,1].detach(), 'b')

layer.py
@@ -197,7 +197,10 @@ class layer():
             print('Measurement matrix and measurement noise matrix sizes don\'t match. Please check and try again.')
             quit()
 
-    def f(self, i):
+    def f(self, x, u):
+        self.x = x                                                  # necessary because of the implementation of 'jacobian'/'hessian'
+        self.u = u
+
         # TODO: generalise this to include nonlinear treatments
         try:
             return self.A @ self.x + self.B_u @ self.u + self.B_a @ self.a
@@ -205,7 +208,7 @@ class layer():
             print("Dimensions don't match!")
             return
     
-    def g(self, i):
+    def g(self):
         # TODO: generalise this to include nonlinear treatments
         try:
             return self.F @ self.x + self.G @ self.u
@@ -230,8 +233,8 @@ class layer():
             return
 
     def prediction_errors(self, i):
-        self.eps_v = self.y - self.g(i)
-        self.eps_x = Diff(self.x, self.n, self.e_sim+1) - self.f(i)
+        self.eps_v = self.y - self.g()
+        self.eps_x = Diff(self.x, self.n, self.e_sim+1) - self.f(self.x, self.u)
         self.eps_eta = self.u - self.eta_u
         self.eps_theta = self.theta - self.k_theta()
         self.eps_gamma = self.gamma - self.k_gamma()
@@ -246,9 +249,9 @@ class layer():
 
         self.saveHistoryPredictionErrors(i)
 
-    def free_energy(self, x, v, i):
-        self.x = x
-        self.v = v
+    def free_energy(self, x, u, i):
+        self.x = x                                                  # necessary because of the implementation of 'jacobian'/'hessian'
+        self.u = u
 
         self.prediction_errors(i)
 
@@ -257,18 +260,37 @@ class layer():
                             ((self.n + self.r + self.p + self.h) * torch.log(2*torch.tensor([[math.pi]])) - torch.logdet(self.Pi_z) - torch.logdet(self.Pi_w) - \
                             torch.logdet(self.Pi_theta) - torch.logdet(self.Pi_gamma)).sum())           # TODO: add more terms due to the variational Gaussian approximation?
 
-    def step(self, i):
-        # FIXME: Choose and properly implement a numerical solver (or multiple ones?) Euler-Maruyama, Local linearisation, Milner, etc.
+    def step(self, i, method=1):
+        # methods:
+        # 0: Euler-Maruyama
+        # 1: Local linearisation (Ozaki '92)
 
         # TODO: The following code works (?) for linear functions (and not nonlinear ones) in virtue of the fact that generalised coordinates for linear models are trivial; for nonlinear models, see snippet "from spm_ADEM_diff"
         # TODO: for nonlinear systems, higher embedding orders of y, x, v should contain derivatives of functions f and g
 
-        self.dx = self.f(i) + self.C @ self.w.noise[i,:].unsqueeze(1)
-        self.x = self.x + self.dt * self.dx
+        if method == 0:
+            self.dx = self.f(self.x, self.u) + self.C @ self.w.noise[i,:].unsqueeze(1)
+            self.x = self.x + self.dt * self.dx
+        elif method == 1:
+            inputs = (self.x, self.u)
+            self.J = torch.autograd.functional.jacobian(lambda x, v: self.f(x, v), inputs)      # TODO: wait for a decent implementation of 'hessian' and 'jacobian' on all inputs similar to backward
+            # print(self.J)
+            self.J_x = self.J[0].squeeze()                                                      # (at the moment both functions rely on grad, which requires specifying inputs). If not, to save some 
+            # self.J_u = self.J[1][1].squeeze()                                                 # time, might want to switch backward --> grad and than take jacobian of grad
+            # print(self.J_x)
+
+            self.dx = (torch.matrix_exp(self.dt * self.J_x) - torch.eye(self.sim)) @ self.J_x.pinverse() @ (self.f(self.x, self.u) + self.C @ self.w.noise[i,:].unsqueeze(1))
+            # self.du = (torch.matrix_exp(self.dt * self.J_u) - torch.eye(self.sim)) @ self.J_u.pinverse() @ ??????                         # TODO: should we implement a way to give dynamic equations for inputs too?
+
+            self.x = self.x + self.dt * self.dx
+            # GM.u = GM.u + dt * du
+        else:
+            print('Method not implemented. Please check and try a different method.')
+            quit()
         
-        for j in range(self.e_sim+1):                                                       # FIXME: In a dynamic model with x, x', x'', x''', ..., the last variable does not get updated during integration, i.e., \dot{x} => x = x + x' = x + f(x)
+        for j in range(self.e_sim+1):                                                       # In a dynamic model with x, x', x'', x''', ..., the last variable does not get updated during integration, i.e., \dot{x} => x = x + x' = x + f(x)
             self.x[self.sim*(j+1)-1] = self.dx[self.sim*(j+1)-2]                            # \dot{x'} => x' = x' + x'' = x' + f(x') but x'' = f(x') (there is no x'' = x'' + x''' = x'' + f(x'') equation)
-        self.y = self.g(i) + self.H @ self.z.noise[i,:].unsqueeze(1)
+        self.y = self.g() + self.H @ self.z.noise[i,:].unsqueeze(1)
 
 
         ### from spm_ADEM_diff
@@ -280,15 +302,6 @@ class layer():
         # end
         # u.v{n}  = dg.dv*u.v{n} + dg.dx*u.x{n} + dg.da*u.a{n} + u.z{n};
 
-        # TODO: check how to implement numerical methods for differential equations with pytorch
-        # (remember that we need a dt and, consequently, a way to handle non-discrete steps)
-        # TODO: define block matrix (multivariate case in arbitrary embedding orders) 
-        # for updates of SDEs similar to what can be found in 'Generalised filtering'
-        # def forward(self, method=0):
-        #     return
-            # methods:
-            # 0: Local linearisation
-            # 1: Euler-Maruyama
     
     def setObservations(self, y):                                                                   # TODO: check if there's a more elegant way of doing this
         self.y = y.detach()